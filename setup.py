b163f8b41091b8744878181612bd3f638b8dd3cc
zhiqiuc <65441812+zhiqiuc@users.noreply.github.com>
2020-08-17 12:09:31 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+# Get description from README
+root = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root, 'README.md'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Paper-Viz',
+    version='1.0.0',
+    description='Professional Python Graphs for Scientific Papers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/swsamleo/Paper-Viz',
+    author='CRUISE Group',
+    author_email='wei.shao@rmit.edu.au',
+    license ='GNU GPLv3',
+    classfiers=[ 
+    keywords=[
+        'scientific papers',
+        'professional python graphs'
+        'thesis template'
+        'python'
+        ],
+    packages = find_packages(),
+    include_package_data=True,
+    
